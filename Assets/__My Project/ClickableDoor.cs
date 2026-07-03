ca45b4993ea389e41197bf671aaa42c4c0164354
garrettdiao <garrettdiao@gmail.com>
2026-07-03 13:54:59 +1000

    7031

---
+++ Assets/__My Project/ClickableDoor.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class ClickableDoor : MonoBehaviour
+{
+    public Transform doorPivot;
+    public Transform player;
+
+    public float interactDistance = 3f;
+    public float openAngle = -90f;
+    public float openSpeed = 120f;
+
+    private bool isOpen = false;
+    private Quaternion closedRotation;
+    private Quaternion openRotation;
+
+    void Start()
+    {
+        closedRotation = doorPivot.rotation;
+
+        openRotation = Quaternion.Euler(
+            doorPivot.eulerAngles.x,
+            doorPivot.eulerAngles.y + openAngle,
+            doorPivot.eulerAngles.z
+        );
+    }
+
+    void Update()
+    {
+        Quaternion targetRotation = isOpen ? openRotation : closedRotation;
+
+        doorPivot.rotation = Quaternion.RotateTowards(
+            doorPivot.rotation,
+            targetRotation,
+            openSpeed * Time.deltaTime
+        );
+    }
+
+    void OnMouseDown()
+    {
+        float distance = Vector3.Distance(player.position, transform.position);
+
+        if (distance <= interactDistance)
+        {
+            isOpen = !isOpen;
+            Debug.Log("Door clicked");
+        }
+        else
+        {
+            Debug.Log("Too far");
+        }
+    }
+}
